1f9a8c9a1b85c62c02ba4ebd190297f99984b712
root <root@localhost>
2018-12-15 00:29:23 +0300

    добавлены кнопки

---
+++ buttons.php
@@ -0,0 +1,50 @@
+<html>
+
+<head>
+
+
+</head>
+
+<body>
+<a href="api.php?search=B"> <button>B</button></a> <br>
+
+<a href="api.php?search=CRAI"> <button>CRAI</button></a> <br>
+
+<a href="api.php?search=CRI"> <button>CRI</button></a> <br>
+
+<a href="api.php?search='DSWL"> <button>DSWL</button></a> <br>
+
+<a href="api.php?search=EDUC"> <button>EDUC</button></a> <br>
+
+<a href="api.php?search=ETM"> <button>ETM</button></a> <br>
+
+<a href="api.php?search=GPRE"> <button>GPRE</button></a> <br>
+
+<a href="api.php?search=ITG"> <button>ITG</button></a> <br>
+
+<a href="api.php?search=JNJ"> <button>JNJ</button></a> <br>
+
+<a href="api.php?search=KEY"> <button>KEY</button></a> <br>
+
+<a href="api.php?search=NNA"> <button>NNA</button></a> <br>
+
+<a href="api.php?search=SPB"> <button>SPB</button></a> <br>
+
+<a href="api.php?search=SWKS"> <button>SWKS</button></a> <br>
+
+<a href="api.php?search=UVE"> <button>UVE</button></a> <br>
+
+<a href="api.php?search=WLK"> <button>WLK</button></a> <br>
+
+<a href="api.php?search=YUMC"> <button>YUMC</button></a> <br>
+
+
+
+
+
+
+
+</body>
+
+
+</html>
