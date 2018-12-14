226dc386558664aa6ce233cd42c1590213e152de
root <root@localhost>
2018-12-15 00:05:29 +0300

    запилил метод search

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type:application/json");
-
+error_reporting(E_All);
 require ("Class.php");
 
 
@@ -16,9 +16,41 @@ function get_all($data){
 
 }
 
-if(!empty($_GET['search'])){
-    $obj= new Div();
-    $objParse = $obj->parse();
-    get_all($objParse);}
 
 
+
+
+
+
+
+if(!empty($_GET['search'])) {
+    $kek = new Div();
+    $kekParse = $kek->parse();
+    $kekParse1 = (array)$kekParse;
+
+//echo(json_encode($kekParse1));
+    $tickerName = $_GET['search'];
+    $needle = getTicket($tickerName, $kekParse1);
+    //var_dump($needle);
+    search($needle);
+
+}
+
+function search($data){
+    header("HTTP 1.1 $data");
+    $jResponse = json_encode($data, JSON_PRETTY_PRINT);
+    echo $jResponse;
+
+}
+
+
+function getTicket($needle, $haystack){
+    foreach ($haystack as $key=>$value)
+    {
+            if ($value['ticker'] == $needle) {
+                return $haystack[$key];
+            }
+
+    }
+    return false;
+}
