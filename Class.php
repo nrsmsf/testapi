efe81cd3cbba8cd012b87a48f465f0c583223598
root <root@localhost>
2018-12-14 17:28:21 +0300

    Добавление файлов

---
+++ Class.php
@@ -0,0 +1,40 @@
+<pre>
+<?php
+error_reporting(E_ALL);
+
+class Div
+{
+	public function get(){
+		$data = file_get_contents('http://nrsmsf.ru/oop/div.csv');
+		$data = explode("\r", $data);
+		return $data;
+	}
+
+    public function parse(){
+        $var = $this->get();
+        $arr = [];
+        for($i = 0; $i < count($var); $i++)  {
+            $x = explode(";" , $var[$i]);
+            $x['ticker'] = $x['0'];
+            $x['date_pay'] = $x['1'];
+            $x['amount'] = $x['2'];
+            $x['date_exp'] = $x['3'];
+            unset($x[0], $x[1], $x[2], $x[3]);
+            array_push($arr, $x);
+
+        }
+
+        return($arr);
+
+    }
+
+}
+
+$obj = new Div;
+print_r($obj->parse());
+
+?>
+
+
+
+</pre>
